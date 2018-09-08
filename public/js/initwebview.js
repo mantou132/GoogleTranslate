b134b4d50ed861d5edea0d457f613463c7c409bd
王先硚 <xianqiao.wang@upai.com>
2018-09-08 18:13:59 +0800

    feat(view): copy translate response

--- public/js/initwebview.js
+++ public/js/initwebview.js
@@ -3,7 +3,7 @@
  * 修改完需要 `command + r` 重新加载 Google 翻译页面
  */
 
-const { ipcRenderer, remote } = require('electron');
+const { ipcRenderer, remote, clipboard } = require('electron');
 
 addEventListener('load', () => {
   const valueHistory = {
@@ -89,7 +89,30 @@ addEventListener('load', () => {
 
   const sourceTTS = document.querySelector('.src-tts');
   const responseTTS = document.querySelector('.res-tts');
-  const window = remote.getCurrentWindow();
+  const responseContainer = document.querySelector('.tlid-translation');
+
+  const responseCopy = responseTTS.cloneNode(true);
+  responseCopy.setAttribute('data-tooltip', '复制');
+  responseCopy.setAttribute('aria-label', '复制');
+  responseCopy.classList.remove('res-tts');
+  responseCopy.classList.add('res-copy');
+  responseCopy.addEventListener('click', () => {
+    const response = responseContainer.textContent.trim();
+    if (response) {
+      clipboard.writeText(response);
+      responseCopy.classList.add('done');
+      setTimeout(() => {
+        responseCopy.classList.remove('done');
+      }, 1000);
+    }
+  });
+  responseTTS.after(responseCopy);
+
+  addEventListener('click', () => {
+    setTimeout(() => {
+      if (!getSelection().toString()) sourceTextArea.focus();
+    });
+  });
 
   addEventListener('keydown', (e) => {
     // command + shift + q
@@ -102,24 +125,22 @@ addEventListener('load', () => {
     }
     // command + 1
     if (e.keyCode === 49 && (e.metaKey || e.ctrlKey)) {
-      if (window.isVisible() && sourceTextArea.value) {
-        e.preventDefault();
+      if (sourceTextArea.value) {
         sourceTTS.click();
       }
     }
     // command + 2
     if (e.keyCode === 50 && (e.metaKey || e.ctrlKey)) {
-      if (window.isVisible() && sourceTextArea.value) {
-        e.preventDefault();
+      if (sourceTextArea.value) {
         responseTTS.click();
       }
     }
-  });
-
-  addEventListener('click', () => {
-    setTimeout(() => {
-      if (!getSelection().toString()) sourceTextArea.focus();
-    });
+    // command + 3
+    if (e.keyCode === 51 && (e.metaKey || e.ctrlKey)) {
+      if (responseContainer.textContent.trim()) {
+        responseCopy.click();
+      }
+    }
   });
 
   const sourceLabel = document.querySelector(
@@ -152,11 +173,14 @@ addEventListener('DOMContentLoaded', () => {
   style.innerHTML = `
     #source {
       max-height: none !important;
+      padding-right: 50px !important;
     }
     *:not(.moremenu) {
       box-shadow: none !important;
     }
     ::-webkit-scrollbar,
+    .res-copy div,
+    .go-wrap,
     .ad-panel {
       display: none !important;
     }
@@ -171,6 +195,16 @@ addEventListener('DOMContentLoaded', () => {
     }
     .frame {
       height: 100vh !important;
+    }
+    .jfk-button-flat:focus {
+      border-color: transparent !important;
+    }
+    .res-copy {
+      background: no-repeat center/45% url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAQAAABIkb+zAAABk0lEQVR4Ae3cNVoFQRAEYNwPgDsJvt0nQxM0ejERdgKcG+ARziVwd/cQp2ZfMx9Vc4Ctf91jGIZ5P9W52iBTeqgP4Y5Qyldk6LDc4+WMAKXJuoQXMwRIrz54DJBSufMaEET0wWuArvgOuPAcAEzAIARYhQACCCCgOjeol2nd/e0BTDoNAZomg8ApH0bAAZooC0BxlIADpAsojRJwgGbJlT6YEXBA0ArUxQk4QOfQuhgBB2yg5/8YAQecowCQYA8ACfYAkGAPAAn2AIyAAy5QAEiwB4AEewBIsAdABBwglygAJNgDQII9ACTYA0CCPQAj4IArFAAS7AE4wR7wC4JXgFeCb4AvCM4Aeo0CfkGwB+CDAAL+DOCGAAL+OeCWAAII8BogdwQQQIDfgHsCAAAaAgjAow8EEEAAAQQQ4C8glgAC/Adg0SMTwJE7wLIJYMX0Y1F8BBFngNoyg/sSt5of4y7SH3VAj+NP1mUtqvWXSpPd/zRgPDrXxnKvwxUZMWEkKJBmmQ5xp7qrk9pYnft5C4ZhHgEvy6Z99ElYFAAAAABJRU5ErkJggg==) !important;
+      filter: brightness(100) !important;
+    }
+    .res-copy.done {
+      background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAQAAABIkb+zAAABOElEQVR4Ae3TgUYEYRSG4YW1diNCwAIGUJs5h7q3QIGQhAjdRyIt7EIQBF1FRBcwKXwso+KwZw7v+1/A/3zzmxEREREREREREW3W7ttpYf7hnr9557d1+a/efR+7LszXhKp8nYuqfJ32vCZfx87gw/+jZrc6/wV+UvATgg8f/mKnOv8ZflbwE4IP30/syWejQPNpIv/o2D698/V8GuCv8/jtD7/ThGp8/9CVmlCGvzjY4GtCFf7c33uuXv3/d/aZrTL4yu56rtcr5H/9wIRmks8PTbClJgyWH5jQTBL5gQni2zKbH5iQzw9NGCJf2c1vE/L5gQk+zufHJjz4eOh85Vd9TL/3xxA//xXK8DWhLl8TSvKVXZbma0JdvibU5WtCXb4m1OVrQl2+JhTla8J2+URERERERET0BTMPgAX3NpM4AAAAAElFTkSuQmCC) !important;
     }
   `;
   document.head.append(style);
