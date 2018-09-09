531cb8453ea9c7bd64c189b78c344e4e7b94e690
王先硚 <xianqiao.wang@upai.com>
2018-09-09 19:07:38 +0800

    fix: auto upload check not work

--- src/main/checkForUpdates.js
+++ src/main/checkForUpdates.js
@@ -5,10 +5,10 @@ import pkg from '../../package.json';
 export default async function checkForUpdates() {
   const url = 'https://api.github.com/repos/mantou132/GoogleTranslate/releases/latest'; // prettier-ignore
   const release = await download(url).then(res => JSON.parse(res.toString()));
-  if (release.name > pkg.version) {
+  if (release.tag_name > pkg.version) {
     const notice = new Notification({
       title: 'Google 翻译',
-      body: `发现新版本 (${release.name}) 可用，点击下载最新版本！`,
+      body: `发现新版本 (${release.tag_name}) 可用，点击下载最新版本！`,
     });
     notice.on('click', () => shell.openExternal(url));
     notice.show();
