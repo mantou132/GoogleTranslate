a5757429a2ac417fd5a129d9352364ed4a66432f
王先硚 <xianqiao.wang@upai.com>
2018-09-10 00:52:23 +0800

    fix: check update error

--- src/main/checkForUpdates.js
+++ src/main/checkForUpdates.js
@@ -5,7 +5,7 @@ import pkg from '../../package.json';
 export default async function checkForUpdates() {
   const url = 'https://api.github.com/repos/mantou132/GoogleTranslate/releases/latest'; // prettier-ignore
   const release = await download(url).then(res => JSON.parse(res.toString()));
-  if (release.tag_name > pkg.version) {
+  if (release.tag_name > `v${pkg.version}`) {
     const notice = new Notification({
       title: 'Google 翻译',
       body: `发现新版本 (${release.tag_name}) 可用，点击下载最新版本！`,
