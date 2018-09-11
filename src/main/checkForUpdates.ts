56c43bdecb642aacbe35b4b1e13e69c920c28caa
王先硚 <xianqiao.wang@upai.com>
2018-09-11 20:34:43 +0800

    chore: version check use non v-prefix tag

--- src/main/checkForUpdates.ts
+++ src/main/checkForUpdates.ts
@@ -2,12 +2,17 @@ import { shell, Notification } from 'electron';
 import fetch from 'node-fetch';
 import pkg from '../../package.json';
 
+interface IGithubRelease {
+  // eslint-disable-next-line camelcase
+  tag_name: string;
+}
+
 export default async function checkForUpdates() {
   const url =
     'https://api.github.com/repos/mantou132/GoogleTranslate/releases/latest';
   const res = await fetch(url);
-  const release = await res.json();
-  if (release.tag_name > `v${pkg.version}`) {
+  const release: IGithubRelease = await res.json();
+  if (release.tag_name.replace(/^v/, '') > pkg.version) {
     const notice = new Notification({
       title: 'Google 翻译',
       body: `发现新版本 (${release.tag_name}) 可用，点击下载最新版本！`,
