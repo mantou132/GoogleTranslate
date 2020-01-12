f9dcc8ff1f69d2fcadcb333a807da2ffb1144609
mantou132 <709922234@qq.com>
2020-01-13 00:58:39 +0800

    Fix window enter 'c' when a shortcut is invoked

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -3,11 +3,22 @@ import robotjs from 'robotjs';
 
 import config from '../config';
 
+export async function delay(time: number) {
+  await new Promise(resolve => setTimeout(resolve, time));
+}
+
 export async function getSelectionText() {
   const oldString = clipboard.readText();
   clipboard.writeText('');
-  robotjs.keyTap('c', config.platform === 'darwin' ? 'command' : 'control');
-  await new Promise(resolve => setTimeout(resolve, 300));
+  // window 平台下，ctrl+q 调用此函数时如果 ctrl 弹起的早则会输入 'c'
+  if (config.platform === 'win32') {
+    await delay(200);
+    robotjs.keyTap('c', 'control');
+    await delay(100);
+  } else {
+    robotjs.keyTap('c', 'command');
+    await delay(300);
+  }
   const newString = clipboard.readText();
   clipboard.writeText(oldString);
   return newString;
