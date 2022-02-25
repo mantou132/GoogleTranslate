b5d3b21faafe2f71603cb31ad22699f795e1f130
wangxianqiao <wangxianqiao@cloudfine.com>
2022-02-25 17:55:09 +0800

    Fixed goolge translate

--- src/preload/css.ts
+++ src/preload/css.ts
@@ -32,6 +32,9 @@ export default async () => {
     div[data-enable-toggle-playback-speed][data-location='2'] ~ span,
     /* ad */
     h1#i5 + div c-wiz > h2 ~ div[jsaction] > div[aria-label],
+    /* translate website */
+    body > c-wiz > div:first-child > div:nth-child(2) > c-wiz > div:not([jsname]):first-child,
+    /* install app */
     body > c-wiz nav + div {
       display: none !important;
     }
@@ -41,15 +44,15 @@ export default async () => {
     body {
       background: transparent !important;
       transform: translateX(100%) !important;
+      margin: ${config.platform === 'win32' ? '20px' : '0'} 0 ${config.platform === 'linux' ? '20px' : '0'} 20px !important;
+      filter: drop-shadow(rgba(0, 0, 0, 0.5) 0px 0px 0.5px) !important;
+      box-shadow: 0 1px 10px rgba(0, 0, 0, 0.15) !important;
     }
     /* main */
     body > c-wiz[view] {
       overflow: auto;
       height: 100vh !important;
       background: #f5f5f5 !important;
-      margin: ${config.platform === 'win32' ? '20px' : '0'} 0 ${config.platform === 'linux' ? '20px' : '0'} 20px !important;
-      filter: drop-shadow(rgba(0, 0, 0, 0.5) 0px 0px 0.5px) !important;
-      box-shadow: 0 1px 10px rgba(0, 0, 0, 0.15) !important;
     }
   `;
   document.head.append(style);
