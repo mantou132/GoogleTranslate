5750804ea127ef2d2b3a270c464f7c357c19485c
mantou <709922234@qq.com>
2022-10-16 00:44:00 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export default {
   platform: process.platform,
   isDebug: process.env.NODE_ENV !== 'production',
   translateUrl: 'https://translate.google.com',
-  translateUrlFallback: 'https://translate.google.cn',
+  translateUrlFallback: 'https://translate.google.com.hk',
   repoRawURL: 'https://raw.githubusercontent.com/mantou132/GoogleTranslate/master/src/preload/config.json',
 };
